240c59b63ed9ffb3e64c3bc0e5828af74563e5d6
alivejuicy <kkddhh1998@gmail.com>
2023-05-31 15:32:20 +0900

    Fix: 매핑 id 수정

---
+++ modules/signUpClickEvent.js
@@ -0,0 +1,18 @@
+const USER_NAME = document.getElementById("username_inp");
+const PASSWORD = document.getElementById("password_inp");
+const CONFIRMPASSWORD = document.getElementById("password_cofirm");
+
+USER_NAME.addEventListener("focus", handleFocusFunc);
+PASSWORD.addEventListener("focus", handleFocusFunc);
+USER_NAME.addEventListener("focusout", handleFocusOutFunc);
+PASSWORD.addEventListener("focusout", handleFocusOutFunc);
+CONFIRMPASSWORD.addEventListener("focus", handleFocusFunc);
+CONFIRMPASSWORD.addEventListener("focusout", handleFocusOutFunc);
+
+function handleFocusFunc() {
+  this.parentNode.style.border = "1px solid #67C8C1";
+}
+
+function handleFocusOutFunc() {
+  this.parentNode.style.border = "1px solid #c6c6c6";
+}
--- modules/storage.js
+++ modules/storage.js
@@ -1,48 +1,47 @@
-//import * as val from '../modules/validation';
-
 const USEREMAIL = document.getElementById("username_inp");
 const USERPW = document.getElementById("password_inp");
 const signUpBtn = document.getElementById("signup-btn");
-const currentPlus30Min = addTime(1);
+const currentPlus1Hours = addTime(1);
 
 function addTime(minutes) {
-    const userlogoutTime = new Date();
-    userlogoutTime.setTime(userlogoutTime.getTime() + minutes * 60 * 60 * 1000);
+  const userlogoutTime = new Date();
+  userlogoutTime.setTime(userlogoutTime.getTime() + minutes * 60 * 60 * 1000);
 
-    let hours = userlogoutTime.getHours();
-    let min = userlogoutTime.getMinutes();
-    let seconds = userlogoutTime.getSeconds();
+  let hours = userlogoutTime.getHours();
+  let min = userlogoutTime.getMinutes();
+  let seconds = userlogoutTime.getSeconds();
 
-    const formattedTime = `${hours}:${padZero(min)}:${padZero(seconds)}`;
-    return formattedTime;
+  const formattedTime = `${hours}:${padZero(min)}:${padZero(seconds)}`;
+  return formattedTime;
 }
 
 function padZero(num) {
-    return num.toString().padStart(2, "0");
+  return num.toString().padStart(2, "0");
 }
 
 function registerUser() {
-    let userData = localStorage.getItem("details") ?
-        JSON.parse(localStorage.getItem("details")) : [];
+  let userData = localStorage.getItem("details")
+    ? JSON.parse(localStorage.getItem("details"))
+    : [];
 
-    let userObj = {
-        email: USEREMAIL.value,
-        password: USERPW.value,
-        logoutTime: currentPlus30Min,
-    };
+  let userObj = {
+    email: USEREMAIL.value,
+    password: USERPW.value,
+    logoutTime: currentPlus1Hours,
+  };
 
-    userData.push(userObj);
+  userData.push(userObj);
 
-    if (localStorage) {
-        localStorage.setItem("details", JSON.stringify(userData));
-        alert("회원가입 되셨습니다!");
-    }
+  if (localStorage) {
+    localStorage.setItem("details", JSON.stringify(userData));
+    alert("회원가입 되셨습니다!");
+  }
 
-    USEREMAIL.value = "";
-    USERPW.value = "";
+  USEREMAIL.value = "";
+  USERPW.value = "";
 }
 
 signUpBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    registerUser();
+  e.preventDefault();
+  registerUser();
 });
