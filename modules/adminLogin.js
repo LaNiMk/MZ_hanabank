fc6447c06eedf07e0d562e8351c5d1b2bb80a8be
alivejuicy <kkddhh1998@gmail.com>
2023-06-02 15:10:08 +0900

    Fix: metatag 적용

---
+++ modules/adminLogin.js
@@ -0,0 +1,3 @@
+const loginBtn = document.getElementById("login-btn");
+const USEREMAIL = document.getElementById("username_inp_login");
+const USERPW = document.getElementById("password_inp_login");
