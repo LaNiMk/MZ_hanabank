be54bfb0a8d59e9d896449ef75f54784bb046d0c
MINKYUNG <132539247+LaNiMk@users.noreply.github.com>
2023-06-02 17:51:26 +0900

    최종수정

--- js/FAQ.js
+++ js/FAQ.js
@@ -1,43 +1,44 @@
-const accordionItems = document.querySelectorAll('.accordion-item');
+const accordionItems = document.querySelectorAll(".accordion-item");
 // const accordionContents = document.querySelector('.accordion-content');
 
-accordionItems.forEach(item => {
-    const title = item.querySelector('.accordion-title');
-    const content = item.querySelector('.accordion-content');
-  
-    title.addEventListener('click', () => {
-      content.classList.toggle('active');
-      if (content.classList.contains('active')) {
-        content.style.maxHeight = content.scrollHeight + 'px';
-      } else {
-        content.style.maxHeight = '0';
-      }
-    });
-  });
-  
+accordionItems.forEach((item) => {
+  const title = item.querySelector(".accordion-title");
+  const content = item.querySelector(".accordion-content");
 
-document.getElementById('search-button').addEventListener('click',search);
-document.getElementById('search-input').addEventListener('keyup', function(event) {
-  if (event.keyCode === 13) {
-    search();
-  }
+  title.addEventListener("click", () => {
+    content.classList.toggle("active");
+    if (content.classList.contains("active")) {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } else {
+      content.style.maxHeight = "0";
+    }
+  });
 });
 
+document.getElementById("search-button").addEventListener("click", search);
+document
+  .getElementById("search-input")
+  .addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+      search();
+    }
+  });
+
 function search() {
-  var searchQuery = document.getElementById('search-input').value;
-  var searchResultsContainer = document.getElementById('search-results');
+  var searchQuery = document.getElementById("search-input").value;
+  var searchResultsContainer = document.getElementById("search-results");
 
   searchResultsContainer.innerHTML = "";
 
-  var accordionTitles = document.getElementsByClassName('accordion-title');
+  var accordionTitles = document.getElementsByClassName("accordion-title");
   for (var i = 0; i < accordionTitles.length; i++) {
     var question = accordionTitles[i].textContent;
-    
+
     if (question.includes(searchQuery)) {
-      var resultItem = document.createElement('div');
+      var resultItem = document.createElement("div");
       resultItem.textContent = question;
-      resultItem.classList.add('search-result');
-      resultItem.addEventListener('click', scrollToResult);
+      resultItem.classList.add("search-result");
+      resultItem.addEventListener("click", scrollToResult);
       searchResultsContainer.appendChild(resultItem);
     }
   }
@@ -46,15 +47,18 @@ function search() {
 // 검색 결과 항목을 클릭했을 때 해당 내용으로 페이지 스크롤 이동하는 함수
 function scrollToResult(event) {
   var question = event.target.textContent;
-  
+
   // FAQ 아코디언에서 해당 질문을 찾아서 스크롤 이동
-  var accordionTitles = document.getElementsByClassName('accordion-title');
-  var accordionContents = document.querySelectorAll('.accordion-content');
+  var accordionTitles = document.getElementsByClassName("accordion-title");
+  var accordionContents = document.querySelectorAll(".accordion-content");
   for (var i = 0; i < accordionTitles.length; i++) {
     var itemQuestion = accordionTitles[i].textContent;
-    
+
     if (itemQuestion === question) {
-      accordionTitles[i].scrollIntoView({ behavior: 'smooth', block: 'center' });
+      accordionTitles[i].scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
 
       // var accordionContent = accordionContents[i];
       // if (accordionContent.style.maxHeight) {
@@ -63,9 +67,9 @@ function scrollToResult(event) {
       //   accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
       // }
 
-      accordionTitles[i].classList.toggle('active');
-      accordionContents[i].classList.toggle('active');
-      
+      accordionTitles[i].classList.toggle("active");
+      accordionContents[i].classList.toggle("active");
+
       break;
     }
   }
