130220717b02c7314694d313194ce6c1a8d0d4b2
alivejuicy <kkddhh1998@gmail.com>
2023-06-04 16:57:58 +0900

    Fix

---
+++ modules/AdminClickEvent.js
@@ -0,0 +1,21 @@
+const USER_NAME = document.getElementById("username_inp");
+const USER_EMAIL = document.getElementById("useremail_inp");
+const PASSWORD = document.getElementById("password_inp");
+const PHONE = document.getElementById("phone_inp");
+
+PASSWORD.addEventListener("focus", handleFocusFunc);
+PASSWORD.addEventListener("focusout", handleFocusOutFunc);
+USER_EMAIL.addEventListener("focus", handleFocusFunc);
+USER_EMAIL.addEventListener("focusout", handleFocusOutFunc);
+USER_NAME.addEventListener("focus", handleFocusFunc);
+USER_NAME.addEventListener("focusout", handleFocusOutFunc);
+PHONE.addEventListener("focus", handleFocusFunc);
+PHONE.addEventListener("focusout", handleFocusOutFunc);
+
+function handleFocusFunc() {
+    this.parentNode.style.border = "1px solid #67C8C1";
+}
+
+function handleFocusOutFunc() {
+    this.parentNode.style.border = "1px solid #c6c6c6";
+}
